1f1667df1c5e64c61e897bf6921dc76e5eb6aab8
ffantasma <o_fantasma@hotmail.com.br>
2024-05-17 13:54:03 -0300

    Adicionando aula3 de python3

---
+++ aula3.py
@@ -0,0 +1,25 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> textos
+Strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas simples
+print('Gustavo Nascimento')
+print(1, 'Gustavo "Nascimento"')
+
+# Aspas duplas
+print("Gustavo Nascimento")
+print(2, "Gustavo 'Nascimento'")
+
+
+# Escape
+print("Gustavo \"Nascimento")
+
+
+# /r
+print(r"Gustavo \"Nascimento\"")
